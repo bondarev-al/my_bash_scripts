ede257a010c03178ad2eccdbc8193dcf345399a0
Bondarev A.S <bondarev-al@mail.ru>
2023-04-03 00:35:30 +0300

    Создание папки настроек, доработка шаблона скрипта VPN

--- vpn.sh
+++ vpn.sh
@@ -4,10 +4,25 @@
 
 PROGRAM_NAME=$( basename $0 )
 
+# создание в домашней директории папки с файлами настроек
+settings_dir=$HOME/.vpn_bondarev-al
+if [ ! -d "$settings_dir" ]; then 
+    mkdir "$settings_dir"
+    chmod 700 "$settings_dir"
+fi 
+
 case "$1" in
     start|s|-s) echo start 
         ;;
     end|stop|e|-e) echo stop
+        ;;
+    new|n|-n) echo new
+        ;;
+    list|l|-l|settings) echo settings
+        ;;
+    help|-h|--help) echo help
+        ;;
+    menu|m|-m) echo menu
         ;;
     *) echo "$PROGRAM_NAME: Invalid parameters." >&2 
         exit 1 ;;
