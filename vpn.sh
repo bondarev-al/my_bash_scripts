78103dd2807f526d3afcfa056edd1446b18bb0c3
Bondarev A.S <bondarev-al@mail.ru>
2023-04-23 23:00:13 +0300

    Реализация создания нового файла подключения

--- vpn.sh
+++ vpn.sh
@@ -29,14 +29,42 @@ case "$1" in
         fi
         ;;
     end|stop|e|-e) echo stop
-        cd "$settings_dir"
-        if [ -e "${vpn_file_pref}${vpn_file}" ]; then
+        if [ -e "${settings_dir}/${vpn_file_pref}${vpn_file}" ]; then
             # читаем найстройки vpn из файла
-            read config < ${vpn_file_pref}${vpn_file}
+            read config < ${settings_dir}/${vpn_file_pref}${vpn_file}
 	    openvpn3 session-manage --config "$config" --disconnect
 	fi
         ;;
     new|n|-n) echo new
+	# Ввод имени файла для настроек vpn    
+        read -p "Enter name for vpn settings: " name 
+	while [[ ! "$name" =~ ^[[:alnum:]_-]+$ ]]; do
+		echo "$PROGRAM_NAME: Invalid name. Use [A-Za-z0-9_-]."
+		read -p "Enter name for vpn settings: " name 
+	done
+	# Проверка есть ли такой файл, если есть, то уточняется нужно ли его перезаписывать
+        if [ -e "${settings_dir}/${vpn_file_pref}${name}" ]; then
+		read -p "$PROGRAM_NAME: Such filename exist. Do you want to overwrite it? [yes/no] "
+		if [[ ! "$REPLY" =~ ^(yes|y)$ ]]; then
+			echo "$PROGRAM_NAME: Error. Such filename exist." >&2
+			exit 2
+		fi
+	fi
+	# Ввод пути к файлу .ovpn
+	read -p "Enter path to .ovpn file: " ovpn_file
+	while [ ! -e "$ovpn_file" ]; do
+		echo "$PROGRAM_NAME: '$ovpn_file' - such file don't exist."
+		read -p "Enter path to .ovpn file: " ovpn_file
+	done
+	# Импорт настроек в openvpn3
+	openvpn3 config-import --config "$ovpn_file" --name "$name" --persistent
+	# Ввод логина и пароля для подключения
+	read -p "Enter login for vpn: " login
+	read -sp "Enter password for vpn: " password && echo ""
+	# Запись настроек в файл
+	echo "$name" > "${settings_dir}/${vpn_file_pref}${name}"
+	echo "$login" >> "${settings_dir}/${vpn_file_pref}${name}"
+	echo "$password" >> "${settings_dir}/${vpn_file_pref}${name}"
         ;;
     list|l|-l|settings) echo settings
         ;;
@@ -44,7 +72,7 @@ case "$1" in
         ;;
     menu|m|-m) echo menu
         ;;
-    *) echo "$PROGRAM_NAME: Invalid parameters." >&2 
+    *) echo "$PROGRAM_NAME: Error. Invalid parameters." >&2 
         exit 1 ;;
 esac
 
